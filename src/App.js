e9fe44da5725f7166b2d4f347289a18a87661ad7
Md. Ajgar Hossain (Jewell) <mdajgarhossain1@gmail.com>
2022-10-23 15:30:48 +0600

    Set dummy data

--- src/App.js
+++ src/App.js
@@ -1,10 +1,45 @@
-import logo from './logo.svg';
 import './App.css';
+import Sunburst from 'sunburst-chart';
+import { useEffect, useRef } from 'react';
 
 function App() {
+  const ref = useRef()
+  const data = {
+    name: "root",
+    children: [
+      {
+        name: "leafA",
+        value: 3
+      },
+      {
+        name: "nodeB",
+        children: [
+          {
+            name: "leafBA",
+            value: 5
+          },
+          {
+            name: "leafBB",
+            value: 1
+          }
+        ]
+      }
+    ]
+  }
+
+  const myChart = Sunburst();
+
+  useEffect(() => {
+   data && myChart
+    .data(data)
+    (ref.current)
+  }, [data])
+
+  console.log("ref", ref);
+  console.log("myChart", myChart.data(data));
   return (
     <div className="App">
-      <header className="App-header">
+      <header className="App-header" ref={ref}>
       </header>
     </div>
   );
