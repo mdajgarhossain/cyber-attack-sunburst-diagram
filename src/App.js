f3b6ccbf85f9d168810ab71afc66016ee27f257a
Md. Ajgar Hossain (Jewell) <mdajgarhossain1@gmail.com>
2022-11-01 23:10:43 +0600

    Removed unused codes

--- src/App.js
+++ src/App.js
@@ -5,29 +5,6 @@ import { useEffect, useRef } from 'react';
 
 function App() {
   const ref = useRef()
-  // const data = {
-  //   name: "root",
-  //   children: [
-  //     {
-  //       name: "leafA",
-  //       value: 3
-  //     },
-  //     {
-  //       name: "nodeB",
-  //       children: [
-  //         {
-  //           name: "leafBA",
-  //           value: 5
-  //         },
-  //         {
-  //           name: "leafBB",
-  //           value: 1
-  //         }
-  //       ]
-  //     }
-  //   ]
-  // }
-
   const data = {
     name: "ATT&CK TREE",
     children: [
@@ -90,8 +67,6 @@ function App() {
     (ref.current).width(500).height(500)
   }, [data])
 
-  console.log("ref", ref);
-  console.log("myChart", myChart.data(data));
   return (
     <div className="App">
       <div className='headline'>
